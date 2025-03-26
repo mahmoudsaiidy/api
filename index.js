8d49410af0c3c0ee5c776f82f1c62973415c951e
Mahmoud El Saidy <90093238+mahmoudsaiidy@users.noreply.github.com>
2025-03-26 16:17:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function Update(email, message, response) {
 app.post('/chat-with-ai', async (req, res) => {
     try {
         const { email, message } = req.body;
-        const response = await axios.post("https://2d48-34-125-90-17.ngrok-free.app/chat", { message });
+        const response = await axios.post("https://447d-23-251-152-8.ngrok-free.app/chat", { message });
 
         if(typeof response.data === 'string'){
             response.data = JSON.parse(response.data)
